a64df649d8c41c79a158c0e19e7a3f8430b4c852
Fevzi <fevziyildirmm@gmail.com>
2021-01-29 18:43:57 +0300

    Updates

--- src/redux/actions/movieChangeActions.js
+++
@@ -1,5 +0,0 @@
-import * as actionTypes from "./actionTypes"
-
-export function movieChangeSuccess(movie){
-    return {type:actionTypes.GET_MOVIE_CHANGE_SUCCESS, payload:movie}
-}
--- src/redux/actions/showChangeAction.js
+++
@@ -1,5 +0,0 @@
-import * as actionTypes from "./actionTypes"
-
-export function showChangeSuccess(show){
-    return {type:actionTypes.GET_SHOW_CHANGE_SUCCESS, payload:show}
-}
